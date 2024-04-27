f18cde182207928e650deb1b0e49e1efb9adb6f4
Kustiche <ChernichnyKustik@mail.ru>
2024-04-27 15:37:56 +0300

    Правки в контейнере "Search" и в функционале поиска погоды по избранному

--- js/favoriteWeatherSearch.js
+++ js/favoriteWeatherSearch.js
@@ -2,9 +2,9 @@ import { search } from "./search.js";
 
 export function favoriteWeatherSearch(e) {
   const isDeleteButton = e.target.className === "weather__delete btn-reset";
+  const favorit = e.target.closest(".weather__favorites-item");
 
-  if (!isDeleteButton) {
-    const favorit = e.target.closest(".weather__favorites-item");
+  if (!isDeleteButton && favorit) {
     const favoritText = favorit.querySelector(".weather__city-name");
 
     search(favoritText.textContent);
